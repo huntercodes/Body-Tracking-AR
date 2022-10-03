f7229769a2faa9779ca8ec9ef57ce506b5d36c34
Hunter Downey <hdowneymac@gmail.com>
2022-10-03 16:12:28 -0500

    setupForBodyTracking()

---
+++ Body Tracking AR/ViewContainers/ARViewContainer.swift
@@ -0,0 +1,51 @@
+//
+//  ARViewContainer.swift
+//  Body Tracking AR
+//
+//  Created by hunter downey on 10/3/22.
+//
+
+import SwiftUI
+import ARKit
+import RealityKit
+
+private var bodySkeleton: BodySkeleton?
+private let bodySkeletonAnchor = AnchorEntity()
+
+struct ARViewContainer: UIViewRepresentable {
+    typealias UIViewType = ARView
+    
+    func makeUIView(context: Context) -> ARView {
+        let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
+        
+        arView.setupForBodyTracking()
+        arView.scene.addAnchor(bodySkeletonAnchor)
+        
+        return arView
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {
+        
+    }
+}
+
+extension ARView: ARSessionDelegate {
+    func setupForBodyTracking() {
+        let configuration = ARBodyTrackingConfiguration()
+        self.session.run(configuration)
+        self.session.delegate = self
+    }
+    
+    public func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        for anchor in anchors {
+            if let bodyAnchor = anchor as? ARBodyAnchor {
+                if let skeleton = bodySkeleton {
+                    skeleton.update(with: bodyAnchor)
+                } else {
+                    bodySkeleton = BodySkeleton(for: bodyAnchor)
+                    bodySkeletonAnchor.addChild(bodySkeleton!)
+                }
+            }
+        }
+    }
+}
