995f501d37f9cfb4acdbb5fa0efb25099758b7d6
Hunter Downey <hdowneymac@gmail.com>
2022-10-03 14:55:49 -0500

    Initial Commit

---
+++ Body Tracking AR/Body_Tracking_ARApp.swift
@@ -0,0 +1,17 @@
+//
+//  Body_Tracking_ARApp.swift
+//  Body Tracking AR
+//
+//  Created by hunter downey on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Body_Tracking_ARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Body Tracking AR/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Body Tracking AR
+//
+//  Created by hunter downey on 10/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
